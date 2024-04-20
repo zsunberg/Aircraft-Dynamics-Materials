a81e56e970a79c905321f5bb8b3732eadcd9a595
Zachary Sunberg <zachary.sunberg@colorado.edu>
2024-04-20 17:47:23 -0600

    added P4

---
+++ assignments/P4/TEMPLATE_controls.m
@@ -0,0 +1,38 @@
+function u = controls(t, x)
+% CONTROLS - computes the control inputs for the aircraft given the time and state
+%
+% INPUTS
+%   t = time in seconds
+%   x = [x, y, z, phi, theta, psi, u, v, w, p, q, r] in SI units and radians
+%
+% OUTPUTS
+%   u = [delta_e, delta_a, delta_r, delta_t] radians and number beteween 0 and 1
+
+    delta_e_0 = 0.1068;
+    delta_t_0 = 0.2439;
+
+    % Insert your control gains here
+    kp = nan;
+    ka = nan;
+    kr = nan;
+
+    % Keep these gains the same
+    kth = -3;
+    kq = -1;
+
+    % Keep this pitch angle
+    theta_r = 0.0278;
+
+    % Insert your roll angle here
+    phi_r = nan;
+
+    % Keep these elevator and thrust controllers
+    delta_e = kth*(theta_r-x(5)) + kq*-x(11) + delta_e_0;
+    delta_t = delta_t_0;
+
+    % Insert your lateral controllers here
+    delta_a = nan;
+    delta_r = nan;
+
+    u = [delta_e, delta_a, delta_r, delta_t];
+end
